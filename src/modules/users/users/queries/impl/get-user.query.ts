3cbcad6e9889b5a9e10db6d78020583185c2be97
BirdKeonavong <101718505+Bird9249@users.noreply.github.com>
2024-03-15 13:04:24 +0700

    fix: get user query

--- src/modules/users/users/queries/impl/get-user.query.ts
+++ src/modules/users/users/queries/impl/get-user.query.ts
@@ -1,5 +1,5 @@
 import { IOffsetBasePaginate } from 'src/common/interface/pagination/pagination.interface';
-import { User } from '../../entities';
+import { User } from 'src/modules/users/entities';
 
 export default class GetUserQuery {
   constructor(public readonly paginate: IOffsetBasePaginate<User>) {}
