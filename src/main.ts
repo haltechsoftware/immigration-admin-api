37b9f7a0e31f11d6cbbb4579613612f2a23c3ff0
BirdKeonavong <101718505+Bird9249@users.noreply.github.com>
2024-03-17 21:10:25 +0700

    fix: client folder

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { existsSync, mkdirSync } from 'fs';
 import { AppModule } from './app.module';
 import { ValibotExceptionsFilter } from './common/filters/valibot-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  if (!existsSync('client')) mkdirSync('client');
 
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new ValibotExceptionsFilter(httpAdapter));
