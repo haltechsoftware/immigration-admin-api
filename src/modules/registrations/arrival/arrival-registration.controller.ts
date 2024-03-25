7ac6b3f15a9912f847e4f9482422d4672571a9bd
BirdKeonavong <101718505+Bird9249@users.noreply.github.com>
2024-03-25 22:03:22 +0700

    add: upload passport and visa image

--- src/modules/registrations/arrival/arrival-registration.controller.ts
+++ src/modules/registrations/arrival/arrival-registration.controller.ts
@@ -1,24 +1,59 @@
 // import { ArrivalRegistrationService } from './arrival-registration.service';
-import { Controller, Get, Param, Query } from "@nestjs/common";
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import { Public } from "src/common/decorators/public.decorator";
-import ArrivalRegisterQuery from "./queries/impl/arrival.query";
+import { FormDataRequest } from 'nestjs-form-data';
+import { Public } from 'src/common/decorators/public.decorator';
+import { Valibot } from 'src/common/decorators/valibot/valibot.decorator';
+import { Output } from 'valibot';
+import { UploadPassportImageCommand } from './commands/impl/upload-passport-image.command';
+import { UploadVisaImageCommand } from './commands/impl/upload-visa-image.command';
+import { UploadPassportImageDto } from './dtos/upload-passport-image.dto';
+import { UploadVisaImageDto } from './dtos/upload-visa-image.dto';
+import ArrivalRegisterQuery from './queries/impl/arrival.query';
 
 @Controller('arrival')
-export class ArrivalRegistrationController{
+export class ArrivalRegistrationController {
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+  ) {}
 
-    constructor(
-        private readonly queryBus: QueryBus,
-      ) {}
+  @Public()
+  @Get()
+  async arrivalRegister(@Query() params: any): Promise<any> {
+    // const { start_date, end_date, entry_name } = params;
 
-    @Public()
-    @Get()
-    async arrivalRegister(@Query() params: any): Promise<any> {
-        // const { start_date, end_date, entry_name } = params;
+    return await this.queryBus.execute<ArrivalRegisterQuery>(
+      new ArrivalRegisterQuery(params),
+    );
+  }
 
-        return await this.queryBus.execute<ArrivalRegisterQuery>(
-            new ArrivalRegisterQuery(params),
-        );
-    }
+  @Public()
+  @Post('upload-passport')
+  @FormDataRequest()
+  async uploadPassport(
+    @Valibot({ schema: UploadPassportImageDto })
+    dto: Output<typeof UploadPassportImageDto>,
+  ): Promise<{ url: string }> {
+    const url = await this.commandBus.execute<
+      UploadPassportImageCommand,
+      string
+    >(new UploadPassportImageCommand(dto));
 
+    return { url };
+  }
+
+  @Public()
+  @Post('upload-visa')
+  @FormDataRequest()
+  async uploadVisa(
+    @Valibot({ schema: UploadVisaImageDto })
+    dto: Output<typeof UploadVisaImageDto>,
+  ): Promise<{ url: string }> {
+    const url = await this.commandBus.execute<UploadVisaImageCommand, string>(
+      new UploadVisaImageCommand(dto),
+    );
+
+    return { url };
+  }
 }
--- src/modules/registrations/arrival/arrival-registration.module.ts
+++ src/modules/registrations/arrival/arrival-registration.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ArrivalRegistrationController } from './arrival-registration.controller';
+import { ArrivalRegistrationRepository } from './arrival-registration.repository';
+import arrivalCommandHandlers from './commands/handlers';
 import arrivalRegisterHandlers from './queries/handlers';
 
 @Module({
-    providers: [...arrivalRegisterHandlers],
-    controllers:[ArrivalRegistrationController],
+  providers: [
+    ...arrivalCommandHandlers,
+    ...arrivalRegisterHandlers,
+    ArrivalRegistrationRepository,
+  ],
+  controllers: [ArrivalRegistrationController],
 })
 export class ArrivalRegistrationModule {}
