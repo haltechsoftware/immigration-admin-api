2e80b5c2caf8f45b10a23f465de92a6879653c6d
BirdKeonavong <101718505+Bird9249@users.noreply.github.com>
2024-04-02 10:39:08 +0700

    update: feedback

--- src/modules/feedback/feedback/command/handlers/delete-feedback.handler.ts
+++ src/modules/feedback/feedback/command/handlers/delete-feedback.handler.ts
@@ -1,6 +1,7 @@
-import { NotFoundException } from '@nestjs/common';
+import { Inject, NotFoundException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { NodeFileUploadService } from 'src/infrastructure/file-upload/node/node-file-upload.service';
+import { IFileUpload } from 'src/infrastructure/file-upload/file-upload.interface';
+import { FILE_UPLOAD_SERVICE } from 'src/infrastructure/file-upload/inject-key';
 import { FeedbackRepository } from '../../feedback.repository';
 import DeleteFeedbackCommand from '../impl/delete-feedback.command';
 
@@ -10,7 +11,7 @@ export default class DeleteFeedbackHandler
 {
   constructor(
     private readonly repository: FeedbackRepository,
-    private readonly fileUpload: NodeFileUploadService,
+    @Inject(FILE_UPLOAD_SERVICE) private readonly fileUpload: IFileUpload,
   ) {}
 
   async execute({ id }: DeleteFeedbackCommand): Promise<string> {
