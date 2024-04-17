5ec12f365c6019b3b9aeec5d19ac6ff016767ea1
BirdKeonavong <101718505+Bird9249@users.noreply.github.com>
2024-04-17 19:42:18 +0700

    update: check point entity

--- src/modules/checkpoints/entities/checkpoints.ts
+++ src/modules/checkpoints/entities/checkpoints.ts
@@ -1,7 +1,6 @@
 import { relations } from 'drizzle-orm';
 import {
   integer,
-  numeric,
   pgTable,
   serial,
   text,
@@ -10,6 +9,8 @@ import {
 } from 'drizzle-orm/pg-core';
 import { checkpointCategories } from './checkpoint_categories';
 import { checkpointTranslate } from './checkpoint_translate';
+import { countries } from './countries';
+import { provinces } from './provinces';
 
 export const checkpoints = pgTable('checkpoints', {
   id: serial('id').primaryKey().notNull(),
@@ -20,9 +21,16 @@ export const checkpoints = pgTable('checkpoints', {
       onUpdate: 'no action',
     },
   ),
+  country_id: integer('country_id').references(() => countries.id, {
+    onDelete: 'cascade',
+    onUpdate: 'no action',
+  }),
+  province_id: integer('province_id').references(() => provinces.id, {
+    onDelete: 'cascade',
+    onUpdate: 'no action',
+  }),
   image: text('image').notNull(),
-  latitude: numeric('latitude', { precision: 10, scale: 6 }).notNull(),
-  longitude: numeric('longitude', { precision: 10, scale: 6 }).notNull(),
+  link_map: text('link_map').notNull(),
   phone_number: varchar('phone_number', { length: 50 }),
   email: varchar('email', { length: 255 }),
   created_at: timestamp('created_at', { mode: 'string' })
@@ -37,6 +45,14 @@ export const checkpointRelations = relations(checkpoints, ({ one, many }) => ({
   category: one(checkpointCategories, {
     fields: [checkpoints.category_id],
     references: [checkpointCategories.id],
+  }),
+  country: one(countries, {
+    fields: [checkpoints.country_id],
+    references: [countries.id],
+  }),
+  province: one(provinces, {
+    fields: [checkpoints.province_id],
+    references: [provinces.id],
   }),
   translates: many(checkpointTranslate),
 }));
