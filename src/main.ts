95dc02798b881a1f14dbddfcc832f0e239ffbfbf
BirdKeonavong <101718505+Bird9249@users.noreply.github.com>
2024-04-26 08:56:44 +0700

    f

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new ValibotExceptionsFilter(httpAdapter));
 
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
