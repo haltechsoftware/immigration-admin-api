18f6eb7c343108cab41d8795e01119d2ce91bb5e
phoud <phoud.dev.20@gmail.com>
2025-03-12 17:02:27 +0700

    update new code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,8 @@ export default {
   out: './drizzle',
   driver: 'mysql2',
   dbCredentials: {
-    uri: process.env.DB_URL || 'mysql://root:123456@localhost:3306/immigration_db_v3',
+    uri: process.env.DB_URL,
+    // uri: process.env.DB_URL || 'mysql://root:123456@localhost:3306/immigration_db_v3',
     // host: process.env.DB_HOST || 'localhost',
     // user: process.env.DB_USER || 'root',
     // password: process.env.DB_PASS || '123456',
