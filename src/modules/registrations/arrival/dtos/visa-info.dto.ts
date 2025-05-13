3ceb20cf718d17bbda16637febeb5322b07a705c
phoud <phoud.dev.20@gmail.com>
2025-05-13 11:25:27 +0700

    update code

--- src/modules/registrations/arrival/dtos/visa-info.dto.ts
+++ src/modules/registrations/arrival/dtos/visa-info.dto.ts
@@ -1,34 +1,25 @@
 import { MemoryStoredFile } from "nestjs-form-data";
-import { custom, maxLength, minLength, object, special, string } from "valibot";
+import { custom, maxLength, minLength, object, optional, special, string } from "valibot";
 
 export const VisaInfoSchema = object({
-  visaCategory: string('Visa must be a string.', [
-      minLength(1, 'Please enter Visa.'),
+  visaCategory: optional(
+    string('Visa must be a string.', [
       maxLength(
         255,
         'Please enter Visa with a maximum length of ${255} characters.',
       ),
-  ]),
-  no: string('Visa No must be a string.', [
-      minLength(1, 'Please enter Visa No.'),
-  ]),
-  date_of_issue: string('Date of issue must be a string.', [
-      minLength(
-        1,
-        'Please enter Date of issue',
-      ),
-  ]),
-  place_of_issue: string('Place of issue must be a string.', [
-        minLength(
-          1,
-          'Please enter Place of issue ',
-        ),
-        maxLength(
-          255,
-          'Please enter Place of issue with a maximum length of 255 characters.',
-        ),
     ]),
-    image: string('image must be a string'),
+  ),
+  no: optional(string('Visa No must be a string.', []),),
+  date_of_issue: optional(string('Date of issue must be a string.', []),),
+  place_of_issue: optional(string('Place of issue must be a string.', [
+      maxLength(
+        255,
+        'Please enter Place of issue with a maximum length of 255 characters.',
+      ),
+    ]),
+  ),
+    image: optional(string('image must be a string'),),
 //   image: special(
 //     (input) => input instanceof MemoryStoredFile,
 //     'ຂໍ້ມູນບໍ່ຖືກຕ້ອງ',
