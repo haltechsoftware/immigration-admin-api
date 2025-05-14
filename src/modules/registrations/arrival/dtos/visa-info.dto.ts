9fc1f22bede53bd189828349e8c8de6956240687
phoud <phoud.dev.20@gmail.com>
2025-05-14 14:10:27 +0700

    update code

--- src/modules/registrations/arrival/dtos/visa-info.dto.ts
+++ src/modules/registrations/arrival/dtos/visa-info.dto.ts
@@ -1,38 +1,10 @@
-import { MemoryStoredFile } from "nestjs-form-data";
-import { custom, maxLength, minLength, object, optional, special, string } from "valibot";
+
+import { object, optional, string } from "valibot";
 
 export const VisaInfoSchema = object({
-  visaCategory: optional(
-    string('Visa must be a string.', [
-      maxLength(
-        255,
-        'Please enter Visa with a maximum length of ${255} characters.',
-      ),
-    ]),
-  ),
-  no: optional(string('Visa No must be a string.', []),),
-  date_of_issue: optional(string('Date of issue must be a string.', []),),
-  place_of_issue: optional(string('Place of issue must be a string.', [
-      maxLength(
-        255,
-        'Please enter Place of issue with a maximum length of 255 characters.',
-      ),
-    ]),
-  ),
-    image: optional(string('image must be a string'),),
-//   image: special(
-//     (input) => input instanceof MemoryStoredFile,
-//     'ຂໍ້ມູນບໍ່ຖືກຕ້ອງ',
-//     [
-//       custom(
-//         (input: MemoryStoredFile) =>
-//           ['image/jpeg', 'image/png', 'image/webp'].includes(input.mimeType),
-//         'ກະລຸນາເລືອກໄຟລ໌ JPEG ຫຼື PNG ຫຼື Webp.',
-//       ),
-//       custom(
-//         (input: MemoryStoredFile) => input.size <= 1024 * 1024 * 10,
-//         'ກະລຸນາເລືອກໄຟລ໌ທີ່ນ້ອຍກວ່າ 10 MB.',
-//       ),
-//     ],
-//   ),
+  visaCategory: optional(string('Visa must be a string.'),),
+  no: optional(string('Visa No must be a string.'),),
+  date_of_issue: optional(string('Date of issue must be a string.'),),
+  place_of_issue: optional(string('Place of issue must be a string.'),),
+  image: optional(string('image must be a string'),),
 });
