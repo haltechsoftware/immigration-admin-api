1cc47641c7c36132e4567662b64f7fce5be6967e
phoud <phoud.dev.20@gmail.com>
2025-06-26 22:24:41 +0700

    update return

--- src/modules/registrations/departure/commands/handlers/departure-registration.handler.ts
+++ src/modules/registrations/departure/commands/handlers/departure-registration.handler.ts
@@ -1,39 +1,48 @@
-import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { DepartureRepository } from "../../departure-registration.repository";
-import { validateDate } from "src/common/utils/validate-date";
-import { UnprocessableEntityException, InternalServerErrorException } from "@nestjs/common";
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { DepartureRepository } from '../../departure-registration.repository';
+import { validateDate } from 'src/common/utils/validate-date';
+import {
+  UnprocessableEntityException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import DepartureRegistrationCommand from '../impl/departure-registration.command';
 
 @CommandHandler(DepartureRegistrationCommand)
 export default class DepartureRegistrationHandler
   implements ICommandHandler<DepartureRegistrationCommand, string>
 {
-  constructor(
-    private readonly repository: DepartureRepository,
-  ) {}
+  constructor(private readonly repository: DepartureRepository) {}
 
   async execute({ input }: DepartureRegistrationCommand): Promise<string> {
     // Validate dates
-    if (input.personal_info?.date_of_birth) validateDate(input.personal_info.date_of_birth, 'date_of_birth');
-    if (input.passport_info?.expiry_date) validateDate(input.passport_info.expiry_date, 'expiry_date');
-    if (input.passport_info?.date_of_issue) validateDate(input.passport_info.date_of_issue, 'passport_info.date_of_issue');
+    if (input.personal_info?.date_of_birth)
+      validateDate(input.personal_info.date_of_birth, 'date_of_birth');
+    if (input.passport_info?.expiry_date)
+      validateDate(input.passport_info.expiry_date, 'expiry_date');
+    if (input.passport_info?.date_of_issue)
+      validateDate(
+        input.passport_info.date_of_issue,
+        'passport_info.date_of_issue',
+      );
 
     // Check if the passport already exists
-    const passports = await this.repository.getPassportAll(input.passport_info.no);
+    // const passports = await this.repository.getPassportAll(
+    //   input.passport_info.no,
+    // );
 
-    if (passports.length > 0) {
-    // Loop through each passport sequentially to check arrival verification status
-        for (const passport of passports) {
-            // Check if the arrival registration exists and is not verified
-            const arrival = await this.repository.findVerifiedAt(passport.id);
+    // if (passports.length > 0) {
+    //   // Loop through each passport sequentially to check arrival verification status
+    //   for (const passport of passports) {
+    //     // Check if the arrival registration exists and is not verified
+    //     const arrival = await this.repository.findVerifiedAt(passport.id);
 
-            if (!arrival || arrival.verified_at == null) {
-                throw new UnprocessableEntityException(
-                    'ການລົງທະບຽນອອກບໍ່ໄດ้ຖືກຢືນຢັນ. ກະລຸນາຢືນຢັນກ່ອນ.'
-                );
-            }
-        }
-    }
+    //     if (!arrival || arrival.verified_at == null) {
+    //       throw new UnprocessableEntityException(
+    //         'ການລົງທະບຽນອອກບໍ່ໄດ้ຖືກຢືນຢັນ. ກະລຸນາຢືນຢັນກ່ອນ.',
+    //       );
+    //     }
+    //   }
+    // }
 
     return await this.repository.create({ input });
   }
--- src/modules/registrations/departure/departure-registration.repository.ts
+++ src/modules/registrations/departure/departure-registration.repository.ts
@@ -3,7 +3,11 @@ import { format } from 'date-fns';
 import { eq } from 'drizzle-orm';
 import { DateTimeFormat } from 'src/common/enum/date-time-fomat.enum';
 import { DrizzleService } from 'src/infrastructure/drizzle/drizzle.service';
-import { departureRegistration, passportInformation, personalInformation } from '../entities';
+import {
+  departureRegistration,
+  passportInformation,
+  personalInformation,
+} from '../entities';
 import DepartureRegistrationCommand from './commands/impl/departure-registration.command';
 import generateVerifyCode from 'src/common/utils/generate-verify-code';
 
@@ -28,7 +32,6 @@ export class DepartureRepository {
       .where(eq(departureRegistration.id, id));
   }
 
-
   async getPassportAll(number: string) {
     return await this.drizzle.db().query.passportInformation.findMany({
       where: (fields, operators) => operators.eq(fields.number, number),
@@ -37,59 +40,83 @@ export class DepartureRepository {
 
   async findVerifiedAt(id: number) {
     return await this.drizzle.db().query.departureRegistration.findFirst({
-      where: (fields, operators) => operators.and(
-        operators.eq(fields.passport_information_id, id),       // Match passport_information_id
-      ),
+      where: (fields, operators) =>
+        operators.and(
+          operators.eq(fields.passport_information_id, id), // Match passport_information_id
+        ),
     });
   }
 
-  async create({ input }: DepartureRegistrationCommand): Promise<string> {
+  async create({ input }: DepartureRegistrationCommand): Promise<any> {
     try {
       const { passport_info, personal_info } = input;
-  
+
       return await this.drizzle.db().transaction(async (tx) => {
         // Insert Personal Information
-        const [personalInfoRes] = await tx
-          .insert(personalInformation)
-          .values({
-            ...personal_info,
-            race: "",
-            family_name: personal_info.family_name,
-            date_of_birth: format(personal_info.date_of_birth, DateTimeFormat.date),
-            phone_number: personal_info.phone_number,
-            place_of_birth: personal_info.place_of_birth,
-          });
-  
+        const [personalInfoRes] = await tx.insert(personalInformation).values({
+          ...personal_info,
+          race: '',
+          family_name: personal_info.family_name,
+          date_of_birth: format(
+            personal_info.date_of_birth,
+            DateTimeFormat.date,
+          ),
+          phone_number: personal_info.phone_number,
+          place_of_birth: personal_info.place_of_birth,
+        });
+
         // Insert Passport Information
-        const [passportInfoRes] = await tx
-          .insert(passportInformation)
-          .values({
-            number: passport_info.no,
-            date_issue: format(passport_info.date_of_issue, DateTimeFormat.date),
-            expiry_date: format(passport_info.expiry_date, DateTimeFormat.date),
-            place_issue: passport_info.place_of_issue,
-            image: passport_info.image,
-            people_image: "",
-          });
-  
+        const [passportInfoRes] = await tx.insert(passportInformation).values({
+          number: passport_info.no,
+          date_issue: format(passport_info.date_of_issue, DateTimeFormat.date),
+          expiry_date: format(passport_info.expiry_date, DateTimeFormat.date),
+          place_issue: passport_info.place_of_issue,
+          image: passport_info.image,
+          people_image: '',
+        });
+
         const code = generateVerifyCode(10);
-  
+
         // Insert Departure Information
-        await tx.insert(departureRegistration).values({
-          black_list: "available",
-          departure_name: input?.departure_point || "",
-          last_leaving: input?.last_leaving || "",
+        const [departure] = await tx.insert(departureRegistration).values({
+          black_list: 'available',
+          departure_name: input?.departure_point || '',
+          last_leaving: input?.last_leaving || '',
           personal_information_id: personalInfoRes.insertId, // ✅ Correct access to insertId
           passport_information_id: passportInfoRes.insertId, // ✅ Correct access to insertId
           verification_code: code,
         });
-  
-        return code;
+
+        // return code;
+
+        const [personalInfo] = await tx
+          .select()
+          .from(personalInformation)
+          .where(eq(personalInformation.id, personalInfoRes.insertId))
+          .limit(1);
+
+        const [passportInfo] = await tx
+          .select()
+          .from(passportInformation)
+          .where(eq(passportInformation.id, passportInfoRes.insertId))
+          .limit(1);
+
+        const [departures] = await tx
+          .select()
+          .from(departureRegistration)
+          .where(eq(departureRegistration.id, departure.insertId))
+          .limit(1);
+
+        return {
+          verification_code: code,
+          personal_info: personalInfo,
+          passport_info: passportInfo,
+          departure_registration: departures,
+        };
       });
     } catch (error) {
       console.error(error);
-      throw new Error("Failed to register departure");
+      throw new Error('Failed to register departure');
     }
   }
-  
 }
