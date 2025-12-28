7dcbfe59d07b196adbcb3c69efe11555b816dfb6
phoud <phou991@gmail.com>
2025-12-28 14:47:50 +0700

    report  arrival

--- src/modules/registrations/arrival/queries/handlers/report-arrival-query.repository.ts
+++ src/modules/registrations/arrival/queries/handlers/report-arrival-query.repository.ts
@@ -20,6 +20,35 @@ export class GetReportArrivalHandler
     const oneYearAgo = new Date(today);
     oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
 
+    const allCountArrivalResult = await this.drizzle
+      .db()
+      .select({ value: count() })
+      .from(arrivalRegistration);
+
+    const toDayCountArrivalResult = await this.drizzle
+      .db()
+      .select({ value: count() })
+      .from(arrivalRegistration)
+      .where(sql`DATE(${arrivalRegistration.created_at}) = DATE(${today})`);
+
+    // Count tourism arrivals in last 30 days
+    const thirtyDaysCountArrivalResult = await this.drizzle
+      .db()
+      .select({ value: count() })
+      .from(arrivalRegistration)
+      .where(
+        sql`DATE(${arrivalRegistration.created_at}) >= DATE(${thirtyDaysAgo})`,
+      );
+
+    // Count tourism arrivals in last 1 year
+    const oneYearCountArrivalResult = await this.drizzle
+      .db()
+      .select({ value: count() })
+      .from(arrivalRegistration)
+      .where(
+        sql`DATE(${arrivalRegistration.created_at}) >= DATE(${oneYearAgo})`,
+      );
+
     // Count all tourism arrivals (no date filter)
     const allCountResult = await this.drizzle
       .db()
@@ -55,6 +84,10 @@ export class GetReportArrivalHandler
       );
 
     return {
+      total_arrival_today: toDayCountArrivalResult[0].value,
+      total_arrival_month_days: thirtyDaysCountArrivalResult[0].value,
+      total_arrival_one_year: oneYearCountArrivalResult[0].value,
+      total_arrival_all: allCountArrivalResult[0].value,
       total_arrival_tourism_today: todayCountResult[0].value,
       total_arrival_tourism_month_days: thirtyDaysCountResult[0].value,
       total_arrival_tourism_one_year: oneYearCountResult[0].value,
